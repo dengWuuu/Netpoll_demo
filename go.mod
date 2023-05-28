3f147494a95b08628851c985c735b2e99a25397e
wudengwu <wudengwu@bytedance.com>
2023-05-28 15:41:18 +0800

    feat: init netpoll demo

---
+++ go.mod
@@ -0,0 +1,7 @@
+module code.byted.org/wudengwu/Netpoll_demo
+
+go 1.19
+
+require github.com/cloudwego/netpoll v0.3.2
+
+require github.com/bytedance/gopkg v0.0.0-20220413063733-65bf48ffb3a7 // indirect
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cloudwego/netpoll"
+)
+
+func main() {
+	network, address := "tcp", ":8080"
+	listener, err := netpoll.CreateListener(network, address)
+	if err != nil {
+		panic("create net listener failed")
+	}
+	// init event loop
+	eventLoop, _ := netpoll.NewEventLoop(
+		handle,
+		netpoll.WithOnPrepare(prepare),
+		netpoll.WithReadTimeout(time.Second),
+	)
+
+	// start listen loop ...
+	err = eventLoop.Serve(listener)
+	if err != nil {
+		panic("server start err")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = eventLoop.Shutdown(ctx)
+	if err != nil {
+		panic("shutdown err")
+	}
+}
+
+var _ netpoll.OnPrepare = prepare
+var _ netpoll.OnRequest = handle
+
+func prepare(netpoll.Connection) context.Context {
+	return context.Background()
+}
+
+func handle(ctx context.Context, connection netpoll.Connection) error {
+	fmt.Printf("ctx: %v", ctx)
+	reader := connection.Reader()
+	defer func(reader netpoll.Reader) {
+		err := reader.Release()
+		if err != nil {
+			panic("reader release err")
+		}
+	}(reader)
+	msg, _ := reader.ReadString(reader.Len())
+	println(msg)
+	return nil
+}
